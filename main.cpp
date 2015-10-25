e652af3a75d5519bbad43401cc390ad73c9201ab
Tim Windelschmidt <fionera@server.fionera.de>
2015-10-25 22:25:57 +0100

    Initial Release

---
+++ main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+
+using namespace std;
+
+int makefile(unsigned long i)
+{
+    ostringstream Str;
+    Str << i;
+    string fileName(Str.str());
+    std::string fileNameStr = fileName + std::string(".txt");
+
+    std::ofstream file (fileNameStr.c_str());
+
+    file << "0" << std::endl;
+    file.close();
+}
+
+int main()
+{
+    std::cout << "Hello" << std::endl;
+    unsigned long a = 0;
+    while (true) {
+    makefile(a);
+    std::cout << a << std::endl;
+    a++;
+    }
+}
